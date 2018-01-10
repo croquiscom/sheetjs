1bef2baf2c26a963e5dd667207f021f189374916
Seoyeong <jybaek06@gmail.com>
2018-01-10 14:09:02 +0900

    0.11.17-cr1
    - change cell style struct => s: {fill, font, border, alignment}
    - fix test code error
    - write style (fill, font, border, alignment)
    - add write option 'sheetStubs'
    - fix -> when read option 'sheetStubs' is true, parsed cell has style
    - typescript typedef about CellStyle
    - typescript typedef add 'sheetStubs' to WritingOptions
    - reference: https://github.com/protobi/js-xlsx

--- types/index.d.ts
+++ types/index.d.ts
@@ -219,6 +219,12 @@ export interface WritingOptions extends CommonOptions {
 
     /** Override workbook properties on save */
     Props?: Properties;
+
+    /** 
+     * Write data for stub cells
+     * @default false
+     */
+    sheetStubs?: boolean;
 }
 
 /** Workbook Object */
@@ -522,6 +528,43 @@ export interface Hyperlink {
     Tooltip?: string;
 }
 
+export interface Fill {
+    patternType?: 'none' | 'solid' | 'mediumGray' | 'darkGray' | 'lightGray' | 'darkHorizontal' | 'darkVertical' | 'darkDown' | 'darkUp' | 'darkGrid' | 'darkTrellis' | 'lightHorizontal' | 'lightVertical' | 'lightDown' | 'lightGrid' | 'lightTrellis' | 'gray125' | 'gray0625';
+    bgColor?: {rgb: string};
+    fgColor?: {rgb: string};
+}
+
+export interface Font {
+    name?: string;
+    sz?: number;
+    family?: number;
+    color?: {rgb: string};
+}
+
+export type BorderStyle = 'none' | 'thin' | 'medium' | 'dashed' | 'dotted' | 'thick' | 'double' | 'hair' | 'mediumDashed' | 'dashDot' | 'mediumDashDot' | 'DashDotDot' | 'mediumDashDotDot' | 'slantDashDot';
+
+export interface Border {
+    left?: {style?: BorderStyle, color?: {rgb: string}};
+    right?: {style?: BorderStyle, color?: {rgb: string}};
+    top?: {style?: BorderStyle, color?: {rgb: string}};
+    bottom?: {style?: BorderStyle, color?: {rgb: string}};
+}
+
+export interface Alignment {
+    vertical?: 'top' | 'right' | 'bottom' | 'left' | 'general';
+    horizontal?: 'top' | 'right' | 'bottom' | 'left' | 'general';
+    textRotation?: string;
+    indent?: string;
+    wrapText?: 'true' | 'false';
+}
+
+export interface CellStyle {
+    fill?: Fill;
+    font?: Font;
+    border?: Border;
+    alignment?: Alignment;
+}
+
 /** Worksheet Cell Object */
 export interface CellObject {
     /** The raw value of the cell.  Can be omitted if a formula is specified */
@@ -558,7 +601,7 @@ export interface CellObject {
     l?: Hyperlink;
 
     /** The style/theme of the cell (if applicable) */
-    s?: any;
+    s?: CellStyle;
 }
 
 /** Simple Cell Address */
